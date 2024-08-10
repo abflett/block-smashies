4af62cb14442d0e075ce9a8febf2fc39702de64c
Adam Flett <adam@adamflett.net>
2024-08-11 00:15:46 +0800

    updated to work on multiple compilers

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include "raylib.h"
 
-typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } GameScreen;
+typedef enum GameScreen
+{
+    LOGO = 0,
+    TITLE,
+    GAMEPLAY,
+    ENDING
+} GameScreen;
 
 int main()
 {
@@ -9,102 +15,108 @@ int main()
 
     bool exitWindowRequested = false;
     bool exitWindow = false;
-	int framesCounter = 0;
+    int framesCounter = 0;
 
-	GameScreen currentScreen = LOGO;
+    GameScreen currentScreen = LOGO;
 
-	InitWindow(screenWidth, screenHeight, "Block Smashies");
-	SetExitKey(KEY_NULL);
+    InitWindow(screenWidth, screenHeight, "Block Smashies");
+    SetExitKey(KEY_NULL);
     SetTargetFPS(60);
 
     while (!exitWindow)
     {
         if (WindowShouldClose() || IsKeyPressed(KEY_ESCAPE))
-			exitWindowRequested = true;
-        
+            exitWindowRequested = true;
+
         if (exitWindowRequested)
         {
             if (IsKeyPressed(KEY_Y))
-				exitWindow = true;
+                exitWindow = true;
             else if (IsKeyPressed(KEY_N))
-				exitWindowRequested = false;
+                exitWindowRequested = false;
         }
 
-        switch(currentScreen)
+        switch (currentScreen)
         {
-            case LOGO:
+        case LOGO:
+        {
+            framesCounter++; // Count frames
+            if (framesCounter > 120)
             {
-                framesCounter++;    // Count frames
-                if (framesCounter > 120)
-                {
-                    currentScreen = TITLE;
-                }
-            } break;
-            case TITLE:
+                currentScreen = TITLE;
+            }
+        }
+        break;
+        case TITLE:
+        {
+            if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
             {
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-                {
-                    currentScreen = GAMEPLAY;
-                }
-            } break;
-            case GAMEPLAY:
+                currentScreen = GAMEPLAY;
+            }
+        }
+        break;
+        case GAMEPLAY:
+        {
+            if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
             {
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-                {
-                    currentScreen = ENDING;
-                }
-            } break;
-            case ENDING:
+                currentScreen = ENDING;
+            }
+        }
+        break;
+        case ENDING:
+        {
+            if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
             {
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-                {
-                    currentScreen = TITLE;
-                }
-            } break;
-            default: break;
+                currentScreen = TITLE;
+            }
+        }
+        break;
+        default:
+            break;
         }
 
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            if (exitWindowRequested)
+        ClearBackground(RAYWHITE);
+        if (exitWindowRequested)
+        {
+            DrawRectangle(0, 100, screenWidth, 200, DARKGRAY);
+            DrawText("Are you sure you want to exit program? [Y/N]", 40, 180, 30, WHITE);
+        }
+        else
+        {
+            switch (currentScreen)
             {
-                DrawRectangle(0, 100, screenWidth, 200, DARKGRAY);
-                DrawText("Are you sure you want to exit program? [Y/N]", 40, 180, 30, WHITE);
+            case LOGO:
+            {
+                DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
+                DrawText("WAIT for 2 SECONDS...", 290, 220, 20, GRAY);
             }
-            else
-			{
-				switch(currentScreen)
-				{
-					case LOGO:
-					{
-						DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
-						DrawText("WAIT for 2 SECONDS...", 290, 220, 20, GRAY);
-
-					} break;
-					case TITLE:
-					{
-						DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
-						DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
-						DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
-
-					} break;
-					case GAMEPLAY:
-					{
-						DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
-						DrawText("GAMEPLAY SCREEN", 20, 20, 40, MAROON);
-						DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
-
-					} break;
-					case ENDING:
-					{
-						DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
-						DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
-						DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
-
-					} break;
-					default: break;
-				}
-			}
+            break;
+            case TITLE:
+            {
+                DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
+                DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
+                DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
+            }
+            break;
+            case GAMEPLAY:
+            {
+                DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
+                DrawText("GAMEPLAY SCREEN", 20, 20, 40, MAROON);
+                DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
+            }
+            break;
+            case ENDING:
+            {
+                DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
+                DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
+                DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
+            }
+            break;
+            default:
+                break;
+            }
+        }
         EndDrawing();
     }
     CloseWindow();
