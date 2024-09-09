a9019ba4538a20b43b93884a7e95363777b4e0bc
Adam Flett <adam@adamflett.net>
2024-09-09 19:51:58 +0800

    added and modified assets

--- src/game_states/playing_state.c
+++ src/game_states/playing_state.c
@@ -1,11 +1,10 @@
+#include <stdlib.h>
 #include "raylib.h"
 #include "box2d/box2d.h"
 #include "kvec.h"
 #include "playing_state.h"
 #include "game_settings.h"
 #include "game_state_manager.h"
-#include "entities.h"
-
 #include "game_context.h"
 
 static GameContext *context = NULL;
